2bbc8123146e0212a901a7cc14e17c233f9a6b7a
Yusuf Çağlar <faf7faf7@gmail.com>
2021-01-27 13:49:25 +0300

    Complete Animating Views and Transactions tutorial in SwiftUI Tutorials from Apple. Hike view codes from Apple, those aren't mine.

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -9,7 +9,8 @@ import Foundation
 import Combine
 
 final class ModelData : ObservableObject {
-    @Published var landmarks : [Landmark] = load("landmarkData.json")
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
